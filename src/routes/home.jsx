0174690400f46e444d986e759580a2f60f09c04d
EvesLastDescendant <clydewitoutabonnie@mail.com>
2025-09-06 17:50:04 +0100

    init

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -5,13 +5,11 @@ import {Services} from "@/custom-components/home/services.jsx";
 import {Footer} from "@/custom-components/home/footer.jsx";
 
 export const Home = () => {
-    const url = import.meta.env.VITE_APP_API_URL
     return (
         <div className={`min-h-screen flex flex-col justify-between items-center gap-10`}>
             <NavigationBar />
             <Header />
             <Policy />
-            <div>url = {url}</div>
             <Services />
             <Footer />
         </div>
